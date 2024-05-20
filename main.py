94f71e5f8fafcc5fc45baa7f44e3c29c4bffe975
Oaj Bidnurkar <oajrockstar@gmail.com>
2024-05-20 16:44:05 +0530

    driver code

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import cv2
 import numpy as np
 import face_recognition
+import pickle
 
 
-img = face_recognition.load_image_file('emma.jpg')
-img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+img = face_recognition.load_image_file("test.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# face = face_recognition.face_locations(img)[0]
-train_encode = face_recognition.face_encodings(img)[0]
+test_encode = face_recognition.face_encodings(img)[0]
+
+with open("encodings.pkl", "rb") as f:
+    faces = pickle.load(f)
 
 
-test = face_recognition.load_image_file('gal.jpg')
-test = cv2.cvtColor(test, cv2.COLOR_BGR2RGB)
-
-test_encode = face_recognition.face_encodings(test)[0]
-
-print(face_recognition.compare_faces([train_encode],test_encode))
-
-print(train_encode)
+for face in faces:
+    if face_recognition.compare_faces([faces[face]], test_encode)[0]:
+        print(f"This is {face}")
